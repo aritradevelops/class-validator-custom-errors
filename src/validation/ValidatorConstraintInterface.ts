8ddca35639cd83bc15f1bc22527aec5c44d229ee
Aritra Sadhukhan <aritra.sadhukhan@codeclouds.co.in>
2024-06-06 14:16:23 +0530

    Dev - passed validatorArguments to validate function

--- src/validation/ValidatorConstraintInterface.ts
+++ src/validation/ValidatorConstraintInterface.ts
@@ -1,4 +1,5 @@
 import { ValidationArguments } from './ValidationArguments';
+import { ValidatorOptions } from './ValidatorOptions';
 /**
  * Custom validators must implement this interface to provide custom validation logic.
  */
@@ -6,7 +7,7 @@ export interface ValidatorConstraintInterface {
   /**
    * Method to be called to perform custom validation over given value.
    */
-  validate(value: any, validationArguments?: ValidationArguments): Promise<boolean> | boolean;
+  validate(value: any, validationArguments?: ValidationArguments, validatorOptions?: ValidatorOptions): Promise<boolean> | boolean;
 
   /**
    * Gets default message when validation for this constraint fail.
